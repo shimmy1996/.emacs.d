7e624f5afc1ff04e35f49a38865532aa8d2f159b
Shimmy Xu <shimmy.xu@shimmy1996.com>
2017-10-17 23:23:05 -0500

    Added packages ox-hugo and magit. Added some aesthetic changes.

--- init.el
+++ init.el
@@ -11,19 +11,6 @@
 
 ;; Load my configs
 ;; ------------------------------------------------------------------------
-(org-babel-load-file "~/.emacs.d/README.org")
-
-;; Auto appended from here on
-;; ------------------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm linum-relative fcitx base16-theme auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (expand-file-name	
+		      "~/.emacs.d/README.org"		
+		      user-emacs-directory))
