d4ed686a182a4428c396fd94b98f6cb0ff08f8b3
Shimmy Xu <shimmy.xu@shimmy1996.com>
2017-12-25 15:38:18 -0600

    Write my own bootstrap function instead of relying on org-babel.

--- init.el
+++ init.el
@@ -1,16 +1,28 @@
-;; Shimmy Xu's Emacs (@shimmy1996)
-;; ------------------------------------------------------------------------
+;;; init.el --- Kickstart Emacs -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Bootstraps the configuration by loading specified org file.
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(require 'org)
-
-;; Load my configs
-;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-                      "~/.emacs.d/README.org"
-                      user-emacs-directory))
+;;; Code:
+(with-temp-buffer
+  (insert-file-contents (expand-file-name "README.org" user-emacs-directory))
+  (goto-char (point-min))
+  (while (not (eobp))
+    (forward-line 1)
+    (cond
+     ;; Skip DISABLED Subtrees
+     ((looking-at "^\\(\\*+\\) \\(DISABLED\\).*$")
+      (end-of-line)
+      (message "%s" (match-string 0))
+      (re-search-forward
+       (format "^\\*\\{1,%d\\} " (length (match-string 1)))
+       nil 1)
+      (beginning-of-line))
+     ;; Log ENABLED Subtrees
+     ((looking-at "^\\*+ \\(ENABLED\\|\\).*$")
+      (message "%s" (match-string 0)))
+     ;; Evaluate Code Blocks
+     ((looking-at "^#\\+BEGIN_SRC +emacs-lisp *$")
+      (let ((l (match-end 0)))
+        (search-forward "\n#+END_SRC")
+        (eval-region l (match-beginning 0)))))))
+;;; init.el ends here
